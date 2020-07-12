4fa5411b4c3589890eeadf6c0b2caa6a5f2d67ac
Mauricio Mas <mauriciomastonon@gmail.com>
2020-07-12 19:11:37 -0300

    Seed fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,10 +16,10 @@ puts "creating new users"
 
 10.times do
   user = User.new(
-    email = Faker::Internet.email,
-    encrypted_password = Faker::Internet.password(min_length: 8),
-    name = Faker::Name.name,
-    location = Faker::Address.street_address
+    email: Faker::Internet.email,
+    password: Faker::Internet.password(min_length: 8),
+    name: Faker::Name.name,
+    location: Faker::Address.street_address
   )
   user.save!
 end
@@ -28,22 +28,27 @@ puts "creating new tools"
 
 10.times do
   tool = Tool.new(
-    name = Faker::Construction.heavy_equipment,
-    category = Faker::Construction.subcontract_category,
-    status = Faker::Boolean.boolean,
-    price = Faker::Number.number(digits: 3),
-    rating = Faker::Number.between(from: 1, to: 5)
+    name: Faker::Construction.heavy_equipment,
+    category: Faker::Construction.subcontract_category,
+    status: true,
+    price: Faker::Number.number(digits: 3),
+    rating: Faker::Number.between(from: 1, to: 5)
   )
+  tool.user = User.all.sample
   tool.save!
 end
 
 puts "creating new reservation"
 
+t1 = Time.parse("2015-11-16 14:40:34")
+t2 = Time.parse("2015-11-20 16:20:23")
 10.times do
   reservation = Reservation.new(
-    status = Faker::Boolean.boolean,
-    start_date = Faker::Date.foward(days: 20),
-    end_start = Faker::Date.foward(days: 10)
+    status: Faker::Boolean.boolean,
+    start_date: rand(t1..t2),
+    end_start: rand(t1..t2)
   )
+  reservation.user = User.all.sample
+  reservation.tool = Tool.all.sample
   reservation.save!
 end
