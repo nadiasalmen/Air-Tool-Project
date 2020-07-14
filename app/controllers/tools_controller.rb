405c4c289f43ab9609de8aab8fa45112f38d469f
Nadia Salmen <nadiasalmen@gmail.com>
2020-07-14 20:49:31 -0300

    Modify routes & ToolsController

---
+++ app/controllers/tools_controller.rb
@@ -0,0 +1,48 @@
+class ToolsController
+  def index
+    @tools = Tool.all
+  end
+
+  def show
+    @tool = Tool.find(params[:id])
+  end
+
+  def new
+    @tool = Tool.new
+  end
+
+  def create
+    @tool = Tool.new(tool_params)
+    if @tool.save
+      redirect_to tool_path(@tool)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @tool = Tool.find(params[:id])
+  end
+
+  def update
+    @tool = Tool.find(params[:id])
+    @tool.update(tool_params)
+    if @tool.save
+      redirect_to tool_path(@tool)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @tool = Tool.find(params[:id])
+    @tool.destroy
+    redirect_to tools_path
+  end
+
+  private
+
+  def tool_params
+    params.require(:tool).permit(:name, :category, :status, :price)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :tools
-  resources :reservations, only: [:index :new :create]
+  resources :tools do
+    resources :reservations, only: [:new, :create]
+  end
+  resources :reservations, only: [:index, :show, :edit, :update]
 end
