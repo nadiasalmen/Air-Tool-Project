caa2c798de5772cf58e795dd250628bfd890694b
Nadia Salmen <nadiasalmen@gmail.com>
2020-07-23 22:21:12 -0300

    Added Cloudinary - Image to Tool and to User - Update Index_tools and nav_bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :avatar])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :avatar])
   end
 
   # Pundit: white-list approach.
